5a320b09292e777b149478443bb0cc1613f088bb
BeepDroid <71675347+BeepDroid@users.noreply.github.com>
2021-11-16 19:22:45 -0600

    Add files via upload

---
+++ IPersonalFinanceApp/IPersonalFinanceApp/src/Account.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kebey
+ */
+public class Account implements IAccount {
+    protected String name;
+    protected double value;
+    
+    public Account(String name, double value){
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String getName() {
+        return name; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getValue() {
+        return value; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
