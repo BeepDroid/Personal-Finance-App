30007160178d64832acb23ac8568a95072d08ae2
MrLaCour1 <93730440+MrLaCour1@users.noreply.github.com>
2021-12-10 15:55:37 -0600

    Update Category.java

--- IPersonalFinanceApp/IPersonalFinanceApp/src/Category.java
+++ IPersonalFinanceApp/IPersonalFinanceApp/src/Category.java
@@ -1,10 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 /**
  *
  * @author Javon LaCour
  */
 public class Category implements ICategory {
-
-    String name;
+   String name;
     TransactionType transaction;
     
     public Category(String name, TransactionType transaction){
@@ -22,7 +26,13 @@ public class Category implements ICategory {
     public TransactionType getType() {
         return transaction; 
     }
-    
+     public void setName(String newName){
+       this.name = newName;
+   } 
+    public void setTransaction(TransactionType transactiontype){
+       this.transaction = transactiontype;  
+    }
     
 }
+  
 
